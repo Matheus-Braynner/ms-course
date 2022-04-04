97f871bf1e06e0bc5a764ee0bac9d2213b5681ff
Matheus Braynner <m.braynner@outlook.com>
2022-04-04 18:30:52 -0300

    Micro service with spring cloud loadbalance and server eureka

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -1,8 +1,13 @@
 package com.devsuperior.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableEurekaClient
+@EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
 
